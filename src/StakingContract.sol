7b19ed2f7c71c71e270b15a26c34d6edd571fa56
aryan <aryankaushik251@gmail.com>
2025-11-29 13:06:08 +0530

    Forge fmt

--- src/StakingContract.sol
+++ src/StakingContract.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
