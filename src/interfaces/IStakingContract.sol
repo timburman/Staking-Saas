3f20b90b52280fdc551ba4b7299554929bf910c8
aryan <aryankaushik251@gmail.com>
2025-12-01 23:18:57 +0530

    IStakingContract: updated IStakingContract for more minimal use

--- src/interfaces/IStakingContract.sol
+++ src/interfaces/IStakingContract.sol
@@ -8,28 +8,6 @@ pragma solidity ^0.8.20;
  */
 interface IStakingContract {
     /**
-     * @notice Common base structure for stake information
-     * @param amount Total amount staked by the user
-     * @param startTime Timestamp when the stake was created
-     * @param lastClaimTime Timestamp of the last reward claim
-     * @param totalRewardsClaimed Cumulative rewards claimed by the user
-     * @param unlockTime Timestamp when tokens can be unstaked (for lock-based)
-     * @param lastRewardCalculation Timestamp of last reward calculation (for ASR)
-     * @param userRewardPerTokenPaid Reward per token paid for user (for ASR)
-     * @param pendingRewards Accumulated but unclaimed rewards (for ASR)
-     */
-    struct BaseStakeInfo {
-        uint256 amount;
-        uint256 startTime;
-        uint256 lastClaimTime;
-        uint256 totalRewardsCalimed;
-        uint256 unlockTime;
-        uint256 lastRewardCalculation;
-        uint256 userRewardPerTokenPaid;
-        uint256 pendingRewards;
-    }
-
-    /**
      * @notice Emitted when a user stakes tokens
      * @param user Address of the staker
      * @param amount Amount of tokens staked
@@ -60,24 +38,12 @@ interface IStakingContract {
      * @notice Unstake staked tokens from the contract
      * @param amount Amount of tokens to unstake
      */
-    function unstake(uint amount) external;
-
-    /**
-     * @notice Claim accumulated rewards
-     */
-    function claimReward() external;
+    function unstake(uint256 amount) external;
 
     /**
      * @notice Get stake information for a specific address
      * @param user Address of the user
      * @return Stake Information
-     */
-    function getStakeInfo(address user) external view returns(BaseStakeInfo);
-
-    /**
-     * @notice Calculate pending rewards for a staker
-     * @param staker Address of the staker
-     * @return Pending reward amount
      */
     function calculateReward(address staker) external view returns (uint256);
 
@@ -92,5 +58,4 @@ interface IStakingContract {
      * @return Address of the staking token
      */
     function stakingToken() external view returns (address);
-
 }
